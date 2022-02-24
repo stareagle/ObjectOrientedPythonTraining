d067ca1ab1ed8c5d989b7a51ac5cc534422cc9cd
Stareagle <stuart@beasley-forrest.net>
2022-02-24 17:00:18 -0500

    Note, Notebook class done. Writing interface

---
+++ MyNoteBook/notebook.py
@@ -0,0 +1,91 @@
+import datetime
+
+# Store the next available id for all new notes
+last_id = 0
+
+
+class Note:
+    """
+    Represent a note in the Notebook.
+    Match against a string in searches and store tags for each note.
+    """
+    def __init__(self, memo, tags=''):
+        """
+        Initialize a note with memo an optional space seperated tags.
+        Automatically set the note's creation date and a unique id
+        :param memo:
+        :param tags:
+        """
+        self.memo = memo
+        self.tags = tags
+        self.creation_date = datetime.date.today()
+        global last_id
+        last_id += 1
+        self.id = last_id
+
+    def match(self, filter):
+        """
+        Determine if this note matches the filter text.
+        Return True if it matches, False otherwise.
+        :param filter:
+        :return: Boolean
+        """
+        return filter in self.memo or filter in self.tags
+
+
+class Notebook:
+    """
+    Represent a collection of notes that can be tagged, modified and searched.
+    """
+    def __init__(self):
+        """
+        Initialize a notebook with an empty list.
+        """
+        self.notes = []
+
+    def new_note(self, memo, tags=''):
+        """
+        Create a new note and add it to the list
+        :param memo:
+        :param tags:
+        :return:
+        """
+        self.notes.append(Note(memo, tags))
+
+    def _find_note(self, note_id):
+        """
+        Locate the note with the given id
+        :param note_id:
+        :return:
+        """
+        for note in self.notes:
+            if note.id == note_id:
+                return note
+            return None
+
+    def modify_memo(self, note_id, memo):
+        """
+        Find the note with the given id and change its memo to the given value.
+        :param note_id:
+        :param memo:
+        :return:
+        """
+        self._find_note(note_id).memo = memo
+
+    def modify_tags(self, note_id, tags):
+        """
+        Find the note with the given id and change its tags to the given value.
+        :param note_id:
+        :param tags:
+        :return:
+        """
+        self._find_note(note_id).tags = tags
+
+    def search(self, filter):
+        """
+        Find all notes that match the given filter string.
+        :param filter:
+        :return: list
+        """
+        return [note for note in self.notes if note.match(filter)]
+
